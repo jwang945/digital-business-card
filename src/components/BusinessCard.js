851f516c683bfa17dcc9bb90ab51a305a3a393ff
Jet Wang <jetwang42@gmail.com>
2023-07-12 19:19:46 -0400

    add img

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -2,7 +2,7 @@ import React from "react"
 export default function BusinessCard() {
     return (
         <div className = "business-card">
-            <img />
+            <img src={require("./../resources/images/maisie-img.jpg")} className="maisie-img"/>
         </div>
     )
 }
