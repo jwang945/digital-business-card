dd7d6e721a558075a7e2517980b3ec2e94d41d18
Jet Wang <jetwang42@gmail.com>
2023-07-12 19:27:33 -0400

    get major components in

--- src/components/BusinessCard.js
+++ src/components/BusinessCard.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Description from "./Description"
 export default function BusinessCard() {
     return (
         <div className = "business-card">
             <img src={require("./../resources/images/maisie-img.jpg")} className="maisie-img"/>
+            <Description />
         </div>
     )
 }
---
+++ src/components/Description.js
@@ -0,0 +1,23 @@
+export default function Description() {
+    return (
+        <div className = "description">
+            <h1 className = "description-name">Maisie Lang</h1>
+            <h2 className = "profession">Certified Cat</h2>
+            <div>
+                <button className = "email-button">Email</button>
+                <button className = "linkedin-button">LinkedIn</button>
+            </div>
+            <div className = "about-description">
+                <h2>About</h2>
+                <h3>About Description</h3>
+            </div>
+            <div className = "interests-description">
+                <h2>Interests</h2>
+                <h3>Interests Description</h3>
+            </div>
+            <div className = "footer">
+                <h2>Footer</h2>
+            </div>
+        </div>
+    )
+}
