8c7d029c864f6fd4f01bf49760b95208713491b7
Jet Wang <jetwang42@gmail.com>
2023-07-12 20:08:31 -0400

    add footer icons and more formatting

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,22 +1,28 @@
 export default function Description() {
     return (
-        <div className = "description">
+        <div className = "description-page">
             <h1 className = "description-name">Maisie Lang</h1>
             <h2 className = "profession">Certified Cat</h2>
-            <div>
-                <button className = "email-button">Email</button>
+            <div className="buttons">
+                <button className = "feed-button">Feed Me</button>
                 <button className = "linkedin-button">LinkedIn</button>
             </div>
-            <div className = "about-description">
-                <h2>About</h2>
-                <h3>About Description</h3>
+            <div className="descriptions-box">
+                <div className = "description">
+                    <h2 className = "about-title">About</h2>
+                    <h3>I am a business cat. I do lots of business. I am adept at catching toys and lasers.</h3>
+                </div>
+                <div className = "description">
+                    <h2>Interests</h2>
+                    <h3>I am interested in big-eye tuna and the mating habits of the common housefly. I am not interested in big and dumb dogs.</h3>
+                </div>
             </div>
-            <div className = "interests-description">
-                <h2>Interests</h2>
-                <h3>Interests Description</h3>
-            </div>
+            
             <div className = "footer">
-                <h2>Footer</h2>
+                <img src={require("./../resources/images/facebook.png")} className="invert"/>
+                <img src={require("./../resources/images/instagram.png")} className="invert"/>
+                <img src={require("./../resources/images/twitter.png")} className="invert"/>
+                <img src={require("./../resources/images/tiktok.png")} className="invert"/>
             </div>
         </div>
     )
